a2a0110ba6552cc09345d7730d932c1a942fda54
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-01-15 18:26:10 -0300

    ex 1 e 2

---
+++ Mundo1_Fundamentos/Exercícios/ex002.py
@@ -0,0 +1,6 @@
+# Faça um programa que leio o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+# Utilizando a saída formatada
+
+nome = input('Qual é o seu nome? ')
+print('É um prazer te conhecer, {}!'.format(nome))
